1b37fbdcb74277e8d99a15df02e3c5e9272fa89b
faramarzamirshahi <faramarzamirshahi@gmail.com>
2020-11-02 17:38:43 -0500

    Added the filter function

--- static/js/app.js
+++ static/js/app.js
@@ -3,6 +3,7 @@ const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 // start building
+// function to add the table to <tbody>
 function buildTable(data) {
     tbody.html(""); // clear the table i.e. create a blank canvas.
     data.forEach((dataRow) => {
@@ -13,3 +14,22 @@ function buildTable(data) {
         });
     });    
 }
+
+// function to filter data
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+};
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
